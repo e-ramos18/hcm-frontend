68fe35ce3289a4a44ebc9d3052e70ae9c6217935
Eric Ramos <ericramosofficial@gmail.com>
2023-09-10 22:46:51 +0800

    Add css margin top

--- src/components/NoTimeLogsMessage.jsx
+++ src/components/NoTimeLogsMessage.jsx
@@ -1,6 +1,6 @@
 const NoTimeLogsMessage = () => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
+    <div className="bg-white p-6 rounded-lg shadow-md mt-2">
       <h3 className="text-lg font-semibold mb-4">No Time Logs Yet!</h3>
       <p className="text-gray-600">
         You haven't logged any time entries yet. Start by logging your working
